0d13cee2593b90b2b818cb2a5cc911102b417bdf
Lee Han Sai <hs960201@gmail.com>
2020-08-19 17:23:27 +0900

    Off-line session

---
+++ blogproject/blog/FAKE/fake.py
@@ -0,0 +1,9 @@
+from faker import Faker
+
+ifake = Faker()
+
+print(ifake.name())
+print(ifake.address())
+print(ifake.text())
+print(ifake.random_number())
+print(ifake.sentence())
---
+++ blogproject/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
+from django.core.paginator import Paginator
+from .models import Post
+
+# Create your views here.
+
+def home(request):
+    posts = Post.objects
+    posts_list=Post.objects.all()
+    paginator = Paginator(posts_list,3)
+    page = request.GET.get('page')
+    posts_num = paginator.get_page(page)
+
+    return render(request,'home.html',{'blogs':posts,'posts_num':posts_num})
+
+def detail(request, post_id):
+    post_detail = get_object_or_404(Post, pk=post_id)
+    return render(request, 'detail.html', {'post':post_detail})
+
+def new(request):
+    return render(request, 'post/new.html')
+
+def create(request):
+    post = Post()
+    post.title = request.GET['title']
+    post.body = request.GET['body']
+    post.pub_date = timezone.datetime.now()
+    post.save()
+    return redirect('/blog/' + str(post.id))
+
+def delete(request, post_id):
+    post_num = get_object_or_404(Post, pk=post_id)
+    post_num.delete()
+    return redirect('/')
