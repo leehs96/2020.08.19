31471716f7e2e87e4dfaf4f7989251c8aa6df88d
Lee Han Sai <hs960201@gmail.com>
2020-08-21 13:41:35 +0900

    전광훈,이만희

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio',
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blog.views
+import portfolio.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +26,7 @@ urlpatterns = [
     path('<int:post_id>/', blog.views.detail, name = "detail"),
     path('blog/create/', blog.views.create, name='create'),
     path('<int:post_id>/delete', blog.views.delete, name="delete"),
-]
+    path('blog/new/', blog.views.new, name='new'),
+    path('portfolio/', portfolio.views.portfolio, name='portfolio'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
